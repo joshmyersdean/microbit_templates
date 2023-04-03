f8f475423bda09c52bbe329f2bce18cbba2326e7
Josh Myers-Dean <42389546+joshmyersdean@users.noreply.github.com>
2023-04-02 20:07:35 -0600

    Starter for for "on start" and "forever" blocks

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
+let bird = game.createSprite(0, 2)
+bird.set(LedSpriteProperty.Blink, 300)
+let obstacles: Array = []
+let index = 0
 basic.forever(function () {
-	
+    while (obstacles.length > 0 && obstacles[0].get(LedSpriteProperty.X) == 0) {
+    	
+    }
 })
